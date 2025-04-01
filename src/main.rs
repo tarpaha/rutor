69e57bfd392f65d9447763cb2e99713074c20e25
tarpaha <ppopchikovsky@gmail.com>
2025-04-01 23:46:56 +0200

    better loader, newer packages

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
 
 mod loader;
+use loader::{Source, load};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let content = loader::load().await?;
+    let content = load(Source::File("page.html".to_string())).await?;
     println!("file size: {} bytes", content.len());
     Ok(())
 }
