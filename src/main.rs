39fadfec1fd2d24a8791b12aa5d193a2461f004a
tarpaha <ppopchikovsky@gmail.com>
2025-04-08 01:10:27 +0200

    parser and first working version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
+use std::collections::HashMap;
 use std::error::Error;
 
 mod loader;
+mod parser;
+use parser::Torrent;
+
 use loader::{Source, load};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let content = load(Source::File("page.html".to_string())).await?;
-    println!("file size: {} bytes", content.len());
+    //let content = load(Source::File("page.html".to_string())).await?;
+    let content = load(Source::Url("https://rutor.info/browse/0/1/0/2".to_string())).await?;
+    let torrents = parser::parse_torrents(&content)?;
+
+    let mut by_name: HashMap<String, Torrent> = HashMap::new();
+    for torrent in &torrents {
+        let title_before_slash = torrent
+            .title
+            .split('/')
+            .next()
+            .unwrap_or("")
+            .trim()
+            .to_string();
+
+        by_name
+            .entry(title_before_slash.to_string())
+            .and_modify(|existing| {
+                if torrent.seeders > existing.seeders {
+                    *existing = torrent.clone();
+                }
+            })
+            .or_insert_with(|| torrent.clone());
+    }
+
+    let mut filtered_torrents: Vec<Torrent> = by_name.into_values().collect();
+    filtered_torrents.sort_by(|a, b| b.seeders.cmp(&a.seeders));
+    for f in filtered_torrents {
+        println!("{}: {}", f.seeders, f.title);
+    }
+
     Ok(())
 }
---
+++ src/parser.rs
@@ -0,0 +1,83 @@
+use std::error::Error;
+use scraper::{ElementRef, Html, Selector};
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+pub struct Torrent {
+    pub title: String,
+    pub url: String,
+    pub comments: usize,
+    pub size: String,
+    pub seeders: usize,
+    pub leechers: usize,
+    pub date: String
+}
+
+pub fn parse_torrents(html_content: &str) -> Result<Vec<Torrent>, Box<dyn Error>> {
+
+    let document = Html::parse_document(html_content);
+
+    let row_selector = Selector::parse("#index table tr")?;
+    let td_selector = Selector::parse("td")?;
+    let movie_selector = Selector::parse("a[href^=\"/torrent/\"]")?;
+
+    let mut movies = Vec::new();
+
+    for row in document.select(&row_selector) {
+
+        let cells: Vec<_> = row.select(&td_selector).collect();
+        if cells.len() != 5 {
+            continue;
+        }
+
+        let date = text_from_element(&cells[0])?;
+
+        let (title, url) = if let Some(movie_element) = cells[1].select(&movie_selector).next() {
+            let name = movie_element.text().collect::<String>().trim().to_string();
+            if name.is_empty() {
+                return Err("Cannot parse movie name".into());
+            }
+            let url = movie_element.value().attr("href").ok_or("Cannot parse movie url")?.to_string();
+            if url.is_empty() {
+                return Err("Cannot parse movie url".into());
+            }
+            (name, url)
+        } else {
+            return Err("Cannot parse movie name and url".into());
+        };
+
+        let comments = text_from_element(&cells[2])?.parse::<usize>()?;
+        let size = text_from_element(&cells[3])?;
+
+        let numbers = text_from_element(&cells[4])?
+            .split_whitespace()
+            .map(|s| s.parse::<usize>())
+            .collect::<Result<Vec<usize>, _>>()?;
+        if numbers.len() != 2 {
+            return Err("Cannot parse seeders and leechers".into());
+        }
+
+        let movie_torrent = Torrent {
+            title,
+            url,
+            comments,
+            size,
+            seeders: numbers[0],
+            leechers: numbers[1],
+            date
+        };
+
+        movies.push(movie_torrent);
+    }
+
+    Ok(movies)
+}
+
+fn text_from_element(element: &ElementRef) -> Result<String, Box<dyn Error>> {
+    let text = element.text().collect::<String>().trim().to_string();
+    if text.is_empty() {
+        Err("Cannot parse element".into())
+    } else {
+        Ok(text)
+    }
+}
