1e3e9aa0871019bfb3ab76599be805ab810e91df
tarpaha <ppopchikovsky@gmail.com>
2025-04-09 01:21:42 +0200

    detailed errors in loader

--- src/loader.rs
+++ src/loader.rs
@@ -11,13 +11,24 @@ pub enum Source {
 pub async fn load(source: Source) -> Result<String, Box<dyn Error>> {
     match source {
         Source::Url(url) => {
-            let content = reqwest::get(url).await?.text().await?;
+            let response = reqwest::get(&url)
+                .await
+                .map_err(|e| format!("Failed to get response from '{}': {}", &url, e))?
+                .error_for_status()
+                .map_err(|e| format!("Server returned an error for '{}': {}", &url, e))?;
+            let content = response.text()
+                .await
+                .map_err(|e| format!("Failed to load data from'{}': {}", &url, e))?;
             Ok(content)
         }
         Source::File(path) => {
-            let mut file = File::open(path).await?;
+            let mut file = File::open(&path)
+                .await
+                .map_err(|e| format!("Could not open file '{}': {}", &path, e))?;
             let mut content = String::new();
-            file.read_to_string(&mut content).await?;
+            file.read_to_string(&mut content)
+                .await
+                .map_err(|e| format!("Could not read file '{}': {}", &path, e))?;
             Ok(content)
         }
     }
