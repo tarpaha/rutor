46755007616788808b503f221a1a448419ebbe96
tarpaha <ppopchikovsky@gmail.com>
2025-04-25 02:06:33 +0200

    template for concurrent receive torrent's description

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::collections::HashMap;
 use std::error::Error;
 use itertools::Itertools;
-
+use tokio::sync::mpsc;
+use tokio::time::{sleep, Duration};
 
 mod loader;
 mod parser;
 use parser::Torrent;
 
 use loader::{Source, load};
+
+const N: usize = 10;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -35,16 +38,49 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .or_insert_with(|| torrent.clone());
     }
 
-    let best_torrents = best_seeders_dict
+    let best_torrents: Vec<Torrent> = best_seeders_dict
         .into_values()
         .sorted_by(|a, b| b.seeders.cmp(&a.seeders))
-        .take(10);
-    
-    for torrent in best_torrents {
-        print_torrent(&torrent);
+        .take(N)
+        .collect();
+
+    let (tx, mut rx) = mpsc::channel::<(usize, String)>(N);
+
+    for idx in 0..best_torrents.len() {
+        let tx = tx.clone();
+        let url = best_torrents[idx].url.clone();
+        tokio::spawn(async move {
+            let description = get_torrent_description(&url).await;
+            let _ = tx
+                .send((idx, description))
+                .await;
+        });
+    }
+    drop(tx);
+
+    let mut buffer: Vec<Option<String>> = vec![None; N];
+
+    let mut next_to_print = 0;
+    while let Some((idx, res)) = rx.recv().await {
+        buffer[idx] = Some(res);
+
+        while next_to_print < N {
+            if let Some(description) = buffer[next_to_print].take() {
+                print_torrent(&best_torrents[next_to_print]);
+                println!("\t{}", description);
+                next_to_print += 1;
+            } else {
+                break;
+            }
+        }
     }
 
     Ok(())
+}
+
+async fn get_torrent_description(url: &String) -> String {
+    sleep(Duration::from_millis(200)).await;
+    url.clone()
 }
 
 fn print_torrent(torrent: &Torrent) {
